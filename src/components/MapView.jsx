d644620d43d830bf1d12d0689316610a01c89d4a
thitisut <pherngmathitisuththi@gmail.com>
2026-05-31 16:20:19 +0700

    แก้ไขเปลี่ยนBase map เป็นภาพดาวเทียม

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -40,11 +40,10 @@ export default function MapView({ activeLayer, selectedDistrict, onDistrictClick
       style={{ width: '100%', height: '100%' }}
       className="z-0"
     >
-      {/* Dark styled basemap */}
+      {/* Satellite basemap */}
       <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        subdomains="abcd"
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+        attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
         maxZoom={19}
       />
 
